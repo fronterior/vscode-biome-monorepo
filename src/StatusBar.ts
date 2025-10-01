6921bd0851e4331a907f52d95c9545a4274ad876
fronterior <lowfronterior@gmail.com>
2025-10-02 03:11:39 +0900

    feat: add status bar icon

---
+++ src/StatusBar.ts
@@ -0,0 +1,36 @@
+import { StatusBarAlignment, type StatusBarItem, window } from 'vscode';
+
+export class StatusBar {
+	private statusBarItem: StatusBarItem;
+
+	constructor() {
+		this.statusBarItem = window.createStatusBarItem(
+			'status',
+			StatusBarAlignment.Right,
+			100,
+		);
+	}
+
+	print(options: Partial<Pick<StatusBarItem, 'text' | 'tooltip' | 'command'>>) {
+		Object.assign(this.statusBarItem, options);
+		this.statusBarItem.name = 'Biome Monorepo';
+		this.statusBarItem.command = 'biome-monorepo.showOutputChannel';
+	}
+
+	reset() {
+		this.print({
+			text: '$(biome-logo)',
+			tooltip: 'Biome Monorepo',
+			command: 'biome-monorepo.showOutputChannel',
+		});
+	}
+
+	start() {
+		this.print({ text: '$(biome-logo)' });
+		this.statusBarItem.show();
+	}
+
+	stop() {
+		this.statusBarItem.hide();
+	}
+}
